785a70e91fb7c6c5df9f1e8adfd336ffcfa91fa3
SUHONG\topic <topic828@Naver.com>
2024-01-16 15:17:10 +0900

    es, cjs , type 대응 package설정

---
+++ packages/themes/build.js
@@ -0,0 +1,39 @@
+import esbuild from 'esbuild';
+import pkg from './package.json' assert { type: 'json' };
+const dev = process.argv.includes('--dev');
+const minify = !dev;
+
+const watch = process.argv.includes('--watch');
+
+const external = Object.keys({
+  ...pkg.devDependencies,
+  ...pkg.peerDependencies,
+});
+
+const baseConfig = {
+  entryPoints: ['src/index.js'],
+  bundle: true,
+  minify,
+  sourcemap: true,
+  outdir: 'dist',
+  target: 'es2019',
+  watch,
+  external,
+};
+
+Promise.all([
+  esbuild.build({
+    ...baseConfig,
+    format: 'esm',
+  }),
+  esbuild.build({
+    ...baseConfig,
+    format: 'cjs',
+    outExtension: {
+      '.js': '.cjs',
+    },
+  }),
+]).catch(() => {
+  console.error('Build failed');
+  process.exit(1);
+});
