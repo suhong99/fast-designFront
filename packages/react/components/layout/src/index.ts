64590a1aaff819c4e54396c52672bd91bc360fa9
SUHONG\topic <topic828@Naver.com>
2024-01-20 11:10:47 +0900

    그리드 추가

--- packages/react/components/layout/src/index.ts
+++ packages/react/components/layout/src/index.ts
@@ -1,6 +1,12 @@
-export { Box, Divider, Flex } from "./layout";
+export { Box, Divider, Flex, Grid, GridItem } from "./layout";
 
-export type { BoxProps, DividerProps, FlexProps } from "./layout";
+export type {
+  BoxProps,
+  DividerProps,
+  FlexProps,
+  GridProps,
+  GridItemProps,
+} from "./layout";
 
 export { Text, Heading } from "./typography";
 export type { TextProps, HeadingProps } from "./typography";
