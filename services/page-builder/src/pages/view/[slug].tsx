17379dc1c547de524a819ed33b7fd6b453f7cc13
SUHONG\topic <topic828@Naver.com>
2024-03-05 00:27:28 +0900

    프리라이트 적용

--- services/page-builder/src/pages/view/[slug].tsx
+++ services/page-builder/src/pages/view/[slug].tsx
@@ -84,6 +84,7 @@ export const getStaticProps: GetStaticProps<{ jsonSchema: Schema }> = async (
 
   // slug - '패스트캠퍼스-온라인-강의-프로모션-100원-이벤트-${viewId}'
   // 맨 마지막에만 아이디 적용하면 됨.  mock
+  //ex) http://localhost:3000/view/%ED%8C%A8%EC%8A%A4%ED%8A%B8%EC%BA%A0%ED%8D%BC%EC%8A%A4-%EC%98%A8%EB%9D%BC%EC%9D%B8-%EA%B0%95%EC%9D%98-%ED%94%84%EB%A1%9C%EB%AA%A8%EC%85%98-100%EC%9B%90-%EC%9D%B4%EB%B2%A4%ED%8A%B8-mock
 
   const slicedSlug = slug.split("-");
   const viewId = slicedSlug[slicedSlug.length - 1];
