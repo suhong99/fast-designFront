556b485e87a86a295d76c85fade5a69046f29688
SUHONG\topic <topic828@Naver.com>
2024-03-06 16:48:57 +0900

    editor

---
+++ services/page-builder/src/pages/editor/new.tsx
@@ -0,0 +1,58 @@
+import { JsonEditor } from "@/src/components/Editor/Json";
+import { DesktopFirstLayout } from "@/src/components/layout/DesktopFirstLayout";
+import { DesktopFirstBody } from "@/src/components/layout/DesktopFirstLayout/Body";
+import { DesktopFirstNav } from "@/src/components/layout/DesktopFirstLayout/Nav";
+import { formatObjectToJson } from "@/src/utils/jsonEditor";
+import { ViewSliceSchemaSnippet } from "@/src/utils/jsonEditor/ViewSchemaSnippet";
+import { previewStorage } from "@/src/utils/storage";
+import { Button } from "@fastcampus/react-components-button";
+import { useState } from "react";
+import ShortUniqueId from "short-unique-id";
+
+const EditorNewPage: React.FC = () => {
+  const { randomUUID } = new ShortUniqueId({ length: 10 });
+  const viewId = randomUUID();
+
+  const [schema, setSchema] = useState(
+    formatObjectToJson(ViewSliceSchemaSnippet.init),
+  );
+
+  const handleReset = () => {
+    setSchema(formatObjectToJson(ViewSliceSchemaSnippet.init));
+  };
+
+  const handlePreview = () => {
+    previewStorage.set(viewId, schema);
+
+    window.open(`/preview/${viewId}`, "_blank");
+  };
+
+  return (
+    <DesktopFirstLayout>
+      <DesktopFirstNav gap={8}>
+        <Button variant="outline" size="md" color="red" onClick={handleReset}>
+          초기화
+        </Button>
+        <Button
+          variant="outline"
+          size="md"
+          color="gray"
+          onClick={handlePreview}
+        >
+          미리보기
+        </Button>
+        <Button size="md" color="green">
+          배포하기
+        </Button>
+      </DesktopFirstNav>
+      <DesktopFirstBody>
+        <JsonEditor
+          value={schema}
+          onChange={(value: string) => setSchema(value || "")}
+        />
+      </DesktopFirstBody>
+    </DesktopFirstLayout>
+  );
+};
+
+export default EditorNewPage;
