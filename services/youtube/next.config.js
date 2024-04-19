cdb4381b8d2f51912ecd329e276724279ef8a6e6
SUHONG\topic <topic828@Naver.com>
2024-04-19 13:26:57 +0900

    nx 빌드 버그 fix

--- services/youtube/next.config.js
+++ services/youtube/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
